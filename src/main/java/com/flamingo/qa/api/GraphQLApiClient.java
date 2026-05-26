7c4c5eaf7d631d0e2568716a474f3cc60a3df263
ilonako <ilonenko24@gmail.com>
2026-05-26 18:40:02 +0300

    feat(api): add GraphQLApiClient for Hygraph Video schema

---
+++ src/main/java/com/flamingo/qa/api/GraphQLApiClient.java
@@ -0,0 +1,32 @@
+package com.flamingo.qa.api;
+
+import com.flamingo.qa.dto.graphql.GraphQLRequestDto;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.springframework.stereotype.Component;
+
+import static io.restassured.RestAssured.given;
+
+/**
+ * Hygraph GraphQL API client.
+ * All operations go through a single POST to the GraphQL endpoint.
+ *
+ * @see <a href="https://hygraph.com/graphql-playground">GraphQL Playground</a>
+ */
+@Component
+public class GraphQLApiClient {
+
+    private static final String GRAPHQL_PATH = "/graphql";
+
+    private final RequestSpecification spec;
+
+    public GraphQLApiClient(ApiSpecFactory specFactory) {
+        this.spec = specFactory.graphQlSpec();
+    }
+
+    public Response execute(GraphQLRequestDto request) {
+        return given().spec(spec)
+                .body(request)
+                .post(GRAPHQL_PATH);
+    }
+}
